7777cb2b488c83dd1964dba69891baa6acaecef7
Otavio Serra <31896900+olserra@users.noreply.github.com>
2021-04-16 16:14:05 +0100

    changed the pages to 100 instead of 300

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,7 +3,7 @@ class Profile < ApplicationRecord
     def self.search(search)
         if !(search == "")
             github = Github.new
-            github.repos.list user: search, per_page: 300
+            github.repos.list user: search, per_page: 100
         else
             return false
         end
