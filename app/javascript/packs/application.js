8bbdbe4b3c301f62c4aa953fbf03d484867cc6fc
Otavio Serra <31896900+olserra@users.noreply.github.com>
2021-04-20 19:39:46 +0100

    fixing heroku build, trying to fix the stimulus error on chrome console

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,11 +7,15 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import { Application } from "stimulus"
-import { definitionsFromContext } from "stimulus/webpack-helpers"
 
 import "bootstrap"
+
+const application = Application.start()
+const context = require.context("./controllers", true, /\.js$/)
+application.load(definitionsFromContext(context))
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+import "controllers"
